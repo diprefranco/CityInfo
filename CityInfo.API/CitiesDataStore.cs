0b624f494ce962fd5f58615d88e6b1ff874537eb
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-11-22 17:28:45 -0300

    CitiesController added.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,34 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>
+            {
+                new CityDto
+                {
+                    Id = 1,
+                    Name = "Rome",
+                    Description = "The capital city of Italy, known for its ancient history and landmarks like the Colosseum."
+                },
+                new CityDto
+                {
+                    Id = 2,
+                    Name = "Milan",
+                    Description = "A global capital of fashion and design, located in northern Italy."
+                },
+                new CityDto
+                {
+                    Id = 3,
+                    Name = "Naples"
+                }
+            };
+        }
+    }
+}
