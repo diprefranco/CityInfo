83a8fa2debb5c2df53d3a2c81162497e82c5943a
diprefranco <39093878+diprefranco@users.noreply.github.com>
2024-11-25 19:05:57 -0300

    FilesController added.

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/files")]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider;
+        }
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            string path = "Azure-Quick-Start-for-NET-Developers.pdf";
+
+            if (!System.IO.File.Exists(path))
+            {
+                return NotFound();
+            }
+
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(path, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(path);
+            return File(bytes, contentType, Path.GetFileName(path));
+        }
+    }
+}
